450841674d390bc7d9ec791e0c2c73c59580e667
anonymousproo <37937624+anonymousproo@users.noreply.github.com>
2020-12-07 02:50:45 -0500

    anonymous pro

---
+++ airmon37.py
@@ -0,0 +1,212 @@
+#this tool provider by Anonymous Pro
+#Author Mahfuzur Rahman
+#python script
+#Follw Me
+#visit: www.anonymousproo.com
+#github: https://github.com/anonymousproo
+#youtube: https://www.youtube.com/anonymousproo
+#Facebook: https://m.facebook.com/anonymousproo1
+
+#!/usr/bin/python
+import os
+import sys, traceback
+os.system('clear')
+
+if os.getuid() != 0:
+	print "Sorry. This script requires root user or python2.7"
+	sys.exit()
+def main():
+	try:
+		print ('''
+
+			  /$$$$$$  /$$                                              /$$$$$$  /$$$$$$$$
+			 /$$__  $$|__/                                             /$$__  $$|_____ $$/
+			| $$  \ $$ /$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$  /$$$$$$$ |__/  \ $$     /$$/ 
+			| $$$$$$$$| $$ /$$__  $$| $$_  $$_  $$ /$$__  $$| $$__  $$   /$$$$$/    /$$/  
+			| $$__  $$| $$| $$  \__/| $$ \ $$ \ $$| $$  \ $$| $$  \ $$  |___  $$   /$$/   
+			\033[1;33m| $$  | $$| $$| $$      | $$ | $$ | $$| $$  | $$| $$  | $$ /$$  \ $$  /$$/    
+			| $$  | $$| $$| $$      | $$ | $$ | $$|  $$$$$$/| $$  | $$|  $$$$$$/ /$$/     
+			|__/  |__/|__/|__/      |__/ |__/ |__/ \______/ |__/  |__/ \______/ \033[1;33m|__/ 
+     
+		          \033[1;36m @ Author: Mahfuzur Rahman | YTB: Anonymous Pro [@\033[1;36m
+			  \033[1;33m @ It has 31 tools inside\033[1;33m
+ 
+''')
+
+		def inicio1():
+			
+				print (''' 
+\033[1;91m 
+@@@@@@@@@@@@@@@@@@@@ +Choose the menu from below+ @@@@@@@@@@@@@@@@@@@@
+\033[1;m
+\033[1;32m 
+\033[1;33m+=======+=======+===========+=========+==========+==========+==================+=============+========+\033[1;33m
+
+{1} @ Magic 			[If you click, you will be taken out of the tool]
+{2} @ Wireless 			[You can enter and take a look]
+{3} @ Quite 			[If you do not use the tool]
+
+\033[1;33m+=======+=======+===========+=========+==========+==========+==================+=============+========+\033[1;33m
+\033[1;32m
+   		      ___________________________________________________________________________________________
+		      |                                                                                         |
+   		      |  {#} Coded By Anonymous Pro YTB                                                    {#}  |
+    		      |  {@} Youtube  - https://www.youtube.com/anonymousproo                              {@}  |
+    		      |  {$} Github   - https://github.com/anonymousproo                                   {$}  |
+    		      |_________________________________________________________________________________________|
+
+			''')
+
+				opcion0 = raw_input("\033[1;36mAnonymousPro\033[5mpress2\033[5m> \033[1;m")
+			
+				while opcion0 == "1":
+	
+					print (''' 
+****************** +Commands+ ******************
+
+
+\033[1;32mAnonymous Pro server is not working\033[1;m 	\033[1;33mPlease try again Later\033[1;m
+
+\033[1;32mecho system for while loop\033[1;m	\033[1;33mplese enter your valied words thank you\033[1;m
+
+		''')
+
+
+				def inicio():
+					while opcion0 == "2":
+						print ('''
+\033[1;36m**************************** Choose the menu from below *****************************\033[1;m
+\033[1;33m+=======+=======+===========+=========+==========+==========+==================+=============+========+\033[1;33m
+
+   		      ___________________________________________________________________________________________
+		      |                                                                                         |
+   		      |  {#} Coded By Anonymous Pro YTB                                                    {#}  |
+    		      |  {@} Youtube  - https://www.youtube.com/anonymousproo                              {@}  |
+    		      |  {$} Github   - https://github.com/anonymousproo                                   {$}  |
+    		      |_________________________________________________________________________________________|
+
+{1} @ Wireless Pentester                                      [Necessary Tools Wireless Attacks]
+{0} @ Install All Tools                                       [If you have enough time]			
+
+
+\033[1;33m+=======+=======+===========+=========+==========+==========+==================+=============+========+\033[1;33m
+
+			 ''')
+						print ("\033[1;32mChoose the menu from the top or press (0) to install all Kali linux tools .\n\033[1;m")
+
+						opcion1 = raw_input("\033[1;36mAnonymousPro\033[5mpress1\033[5m> \033[1;m")
+						if opcion1 == "back":
+							inicio1()
+						elif opcion1 == "gohome":
+							inicio1()
+						elif opcion1 == "0":
+								cmd = os.system("apt-get install -y aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx kalibrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite")
+						while opcion1 == "1":
+							print ('''
+\033[1;33m@#{Necessary Tools Wireless Attacks}@#\033[1;32m
++=========================================================================================================================+
+@	 {1} Aircrack-ng								{17} kalibrate-rtl	       @		
+@	 {2} Asleap									{18} KillerBee		       @
+@	 {3} Bluelog			{#} Coded By Anonymous Pro YTB		        {19} Kismet		       @
+@	 {4} BlueMaho									{20} mdk3		       @
+@	 {5} Bluepot			+============================+			{21} mfcuk		       @
+@	 {6} BlueRanger			|			    |			{22} mfoc		       @
+@	 {7} Bluesnarfer		|	{31} Wifitap	    |			{23} mfterm		       @
+@	 {8} Bully			|			    |			{24} Multimon-NG	       @	
+@	 {9} coWPAtty			+============================+			{25} PixieWPS		       @
+@	{10} crackle									{26} Reaver  		       @
+@	{11} eapmd5pass									{27} redfang          	       @
+@	{12} Fern Wifi Cracker		{#} Coded By Anonymous Pro YTB			{28} RTLSDR Scanner	       @
+@	{13} Ghost Phisher								{29} Spooftooph		       @
+@	{14} GISKismet									{30} Wifi Honey		       @		
+@	{16} gr-scan									{32} Wifite   		       @
+@														       @			
+@				{0} Install all Wireless Attacks tools						       @
++=========================================================================================================================+				 
+						''')
+							print ("\033[1;32mInsert the number of the tool to install it .\n\033[1;32m")
+							opcion2 = raw_input("\033[1;36mmAnonymousPro > \033[1;m")
+							if opcion2 == "1":
+								cmd = os.system("apt-get install aircrack-ng")
+
+							elif opcion2 == "2":
+								cmd = os.system("apt-get install asleap")
+
+							elif opcion2 == "3":
+								cmd = os.system("apt-get install bluelog")
+							elif opcion2 == "4":
+								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/bluemaho.git")
+							elif opcion2 == "5":
+								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/bluepot.git")
+							elif opcion2 == "6":
+								cmd = os.system("apt-get install blueranger")
+							elif opcion2 == "7":
+								cmd = os.system("apt-get install bluesnarfer")
+							elif opcion2 == "8":
+								cmd = os.system("apt-get install bully")
+							elif opcion2 == "9":
+								cmd = os.system("apt-get install cowpatty")
+							elif opcion2 == "10":
+								cmd = os.system("apt-get install crackle")
+							elif opcion2 == "11":
+								cmd = os.system("apt-get install eapmd5pass")
+							elif opcion2 == "12":
+								cmd = os.system("apt-get install fern-wifi-cracker")
+							elif opcion2 == "13":
+								cmd = os.system("apt-get install ghost-phisher")
+							elif opcion2 == "14":
+								cmd = os.system("apt-get install giskismet")
+							elif opcion2 == "16":
+								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/gr-scan.git")
+							elif opcion2 == "17":
+								cmd = os.system("apt-get install kalibrate-rtl")
+							elif opcion2 == "18":
+								cmd = os.system("apt-get install killerbee")
+							elif opcion2 == "19":
+								cmd = os.system("apt-get install kismet")
+							elif opcion2 == "20":
+								cmd = os.system("apt-get install mdk3")
+							elif opcion2 == "21":
+								cmd = os.system("apt-get install mfcuk")
+							elif opcion2 == "22":
+								cmd = os.system("apt-get install mfoc")
+							elif opcion2 == "23":
+								cmd = os.system("apt-get install mfterm")
+							elif opcion2 == "24":
+								cmd = os.system("apt-get install multimon-ng")
+							elif opcion2 == "25":
+								cmd = os.system("apt-get install pixiewps")
+							elif opcion2 == "26":
+								cmd = os.system("apt-get install reaver")
+							elif opcion2 == "27":
+								cmd = os.system("apt-get install redfang")
+							elif opcion2 == "28":
+								cmd = os.system("apt-get install rtlsdr-scanner")
+							elif opcion2 == "29":
+								cmd = os.system("apt-get install spooftooph")
+							elif opcion2 == "30":
+								cmd = os.system("apt-get install wifi-honey")
+							elif opcion2 == "31":
+								cmd = os.system("apt-get install wifitap")
+							elif opcion2 == "32":
+								cmd = os.system("apt-get install wifite")
+							elif opcion2 == "0":
+								cmd = os.system("apt-get install -y aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx kalibrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite")
+							elif opcion2 == "back":
+								inicio()
+							elif opcion2 == "gohome":
+								inicio1()
+							else:
+								print ("\033[1;31mSorry, that was an invalid command!\033[1;m")
+
+				inicio()
+		inicio1()
+	except KeyboardInterrupt:
+		print ("Shutdown requested...Goodbye...Don't Forget To Subscribe Anonymous Pro")
+	except Exception:
+		traceback.print_exc(file=sys.stdout)
+	sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
